a1540126934a5e95c35297b64b730e0bdfbcf6ed
nghianguyend <nghia.dn140206@gmail.com>
2025-09-12 17:02:51 +0200

    commit

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,2 +1,7 @@
 import yfinance as yf
-from datetime import datetime, timedelta
+from datetime import datetime
+
+stock = yf.Ticker("MSFT")
+stock_data = stock.history(period="max")
+
+print(stock_data)
